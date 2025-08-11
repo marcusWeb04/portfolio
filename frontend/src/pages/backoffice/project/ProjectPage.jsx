2970685b2392a92c5df61bda373198d2b0c2312d
marcusWeb04 <marcusfavernay@gmail.com>
2025-08-11 14:47:45 +0200

    ajout de la pop up apreçu sur le dashbord

--- frontend/src/pages/backoffice/project/ProjectPage.jsx
+++ frontend/src/pages/backoffice/project/ProjectPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router";
 import Aside from "../../../components/backoffice/aside";
+import ProjectPopUp from "../../../components/project/ProjectPopUp.jsx";
 import { getProjectPagination } from "../../../service/requestProject";
 
 function ProjectPage(){
@@ -9,6 +10,7 @@ function ProjectPage(){
     const [limit, setLimit] = useState(7);
     const [content, setContent] = useState([]);
     const [projectType, setProjectType] = useState('Tout');
+    const [selectedProject, setSelectedProject] = useState(null);
 
     useEffect(()=>{
         const handleRequest = async () => {
@@ -36,18 +38,41 @@ function ProjectPage(){
                         </tr>
                     </thead>
                     <tbody>
-                        {content.map((project) => (
-                            <tr key={project.id} className="h-[5vh] border-b border-gray-200">
-                                <td className="text-center">{project.id}</td>
-                                <td className="text-center">{project.title}</td>
-                                <td className="text-center">{project.description ?? "pas de description"}</td>
-                                <td className="text-center"><button>Aperçu</button></td>
-                                <td className="text-center"><button onClick={() => navigate(`/projects/edit/${project.id}`)}>Modifier</button></td>
-                                <td className="text-center"><button onClick={() => console.log("Supprimer", project.id)}>Supprimer</button></td>
-                            </tr>
-                        ))}
+                      {content.map((project) => (
+                        <tr
+                          key={project.id}
+                          className="h-[5vh] border-b border-gray-200 cursor-pointer"
+                        >
+                          <td className="text-center">{project.id}</td>
+                          <td className="text-center">{project.title}</td>
+                          <td className="text-center">{project.description}</td>
+                          <td className="text-center">
+                            <button onClick={() => setSelectedProject(project)}>Aperçu</button>
+                          </td>
+                          <td className="text-center">
+                            <button onClick={(element) => {
+                              element.stopPropagation();
+                              navigate(`/projects/edit/${project.id}`);
+                            }}>Modifier</button>
+                          </td>
+                          <td className="text-center">
+                            <button onClick={(e) => {
+                              element.stopPropagation();
+                              console.log("Supprimer", project.id);
+                            }}>Supprimer</button>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                 </table>
+
+                {/* PopUp affiché conditionnellement */}
+                {selectedProject && (
+                  <ProjectPopUp
+                    project={selectedProject}
+                    onClose={() => setSelectedProject(null)}
+                  />
+                )}
 
                 {/* Pagination */}
                 <div className="flex justify-center space-x-2 mt-auto">
